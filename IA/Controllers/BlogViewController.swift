04a369d3a02ee3e4a02a9029b823a609767c145a
snigdhatiwari17 <103492576+snigdhatiwari17@users.noreply.github.com>
2023-04-26 19:14:19 +0530

    setup blog view controller

---
+++ IA/Controllers/BlogViewController.swift
@@ -0,0 +1,22 @@
+//
+//  BlogViewController.swift
+//  IA
+//
+//  Created by Snigdha Tiwari  on 26/04/23.
+//
+
+
+import UIKit
+
+class BlogViewController: UIViewController {
+
+    
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
--- IA/Controllers/WelcomeViewController.swift
+++ IA/Controllers/WelcomeViewController.swift
@@ -10,19 +10,13 @@ import UIKit
 class WelcomeViewController: UIViewController {
 
     
-    @IBOutlet weak var titleLabel: UILabel!
-    override func viewDidLoad() {
-        titleLabel.text = K.appName
+        override func viewDidLoad() {
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-    @IBAction func registerPressed(_ sender: Any) {
-        self.performSegue(withIdentifier: K.toRegisterSegue, sender: self)
-    }
+
     
-    @IBAction func signUpPressed(_ sender: Any) {
-        self.performSegue(withIdentifier: K.toLogInSegue, sender: self)
-    }
 }
 
